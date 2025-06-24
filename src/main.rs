874a03086ebb6276aded019dc20db0899d2b2698
Cary <cary@grokism.com>
2025-06-23 20:40:08 -0500

    Initial commizzle

---
+++ src/main.rs
@@ -0,0 +1,143 @@
+mod token;
+mod symbol_table;
+
+use std::{env, fs};
+use std::process::{exit, Command};
+use crate::token::Token;
+use crate::symbol_table::SymbolTable;
+
+enum InterpretationResult{
+    Ok(String),
+    Incomplete,
+    Err(String)
+}
+
+use InterpretationResult::*;
+
+fn convert_input_name_to_ll(name: &str) -> String {
+    let found_delimiter = name.find(".");
+    match found_delimiter {
+        None => String::from(name) + ".ll",
+        Some(index) => String::from(&name[0..index]) + ".ll"
+    }
+}
+
+fn convert_ll_name_to_o(name: &str) -> String {
+    let found_delimiter = name.find(".");
+    match found_delimiter {
+        None => String::from(name) + ".o",
+        Some(index) => String::from(&name[0..index]) + ".o"
+    }
+}
+
+fn convert_o_name_to_exe(name: &str) -> String {
+    let found_delimiter = name.find(".");
+    match found_delimiter {
+        None => String::from(name) + ".out",
+        Some(index) => String::from(&name[0..index])
+    }
+}
+
+fn interpret_macro(symbols: &mut SymbolTable, carry: &mut Vec<Token>) -> InterpretationResult{
+    println!("Interpreting macro with symbols: {:?} and carry: {:?}", symbols, carry);
+    *carry = Vec::new();
+    Ok("".to_string())
+}
+
+fn execute_macro(symbols: &mut SymbolTable, carry: &mut Vec<Token>) -> InterpretationResult{
+    println!("Executing macro with symbols: {:?} and carry: {:?}", symbols, carry);
+    *carry = Vec::new();
+    Ok("".to_string())
+}
+
+fn interpret_line(line: &str, symbols: &mut SymbolTable, carry: &mut Vec<Token>) -> InterpretationResult {
+    let trimmed = line.trim();
+
+    if trimmed.len() == 0{
+        return if carry.len() == 0 {
+            Ok(String::from(""))
+        } else {
+            Incomplete
+        };
+    }
+
+    if trimmed.starts_with("~"){
+        return if carry.len() == 0 {
+            println!("Current line is llvm: {}", trimmed);
+            Ok(String::from(&trimmed[1..trimmed.len()]))
+        } else {
+            carry.push(Token::from(trimmed));
+            Incomplete
+        }
+    }
+
+    carry.append(&mut Token::separate(trimmed));
+    println!("Carry turned into after tokens separated: {:?}", &carry);
+
+    if carry[0].eq(&Token::from("macro")){
+        interpret_macro(symbols, carry)
+    }else{
+        execute_macro(symbols, carry)
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect::<Vec<String>>();
+    if args.len() < 1 {
+        println!("Please enter a filename.");
+        return;
+    }
+
+    let file_name = &args[0];
+    let file_ll_name = convert_input_name_to_ll(&file_name);
+    let file_o_name = convert_ll_name_to_o(&file_ll_name);
+    let file_exe_name = convert_o_name_to_exe(&file_o_name);
+    let file_contents = fs::read_to_string(&file_name).expect("Something went wrong reading the file");
+
+    let lines: Vec<&str> = file_contents.split("\n").collect();
+    let mut carry: Vec<Token> = Vec::new();
+    let mut interpreted_lines: Vec<String> = Vec::new();
+    let mut symbol_table: SymbolTable = SymbolTable::new();
+    for (index, line) in lines.iter().enumerate() {
+        let interpreted_line_result = interpret_line(line, &mut symbol_table, &mut carry);
+        match interpreted_line_result {
+            Ok(new_line) => {
+                interpreted_lines.push(new_line);
+                continue;
+            },
+            Incomplete =>{
+                continue;
+            },
+            Err(e) => {
+                println!("Error in line number {} with line:\n{}\nError description: {}", &index, &line, e);
+                return;
+            }
+        }
+    }
+
+    fs::write(&file_ll_name, interpreted_lines.join("\n")).expect("Unable to write file");
+
+    println!("LLVM file generated. Compiling to binary.");
+
+    let llc_output = Command::new("llc").args(["-filetype=obj", &file_ll_name, "-o", &file_o_name]).output().expect("Failed to compile llvm code: ");
+    if llc_output.stderr.len() > 0 {
+        println!("LLC Error: {}", String::from_utf8(llc_output.stderr).unwrap());
+        return;
+    }
+
+    //Command::new("rm").args([&file_ll_name]).output().expect("Failed to remove intermediate ll file: ");
+    let clang_output = Command::new("clang").args([&file_o_name, "-o", &file_exe_name]).output().expect("Failed to link: ");
+    if clang_output.stderr.len() > 0 {
+        println!("CLANG Error: {}", String::from_utf8(clang_output.stderr).unwrap());
+        return;
+    }
+
+    Command::new("rm").args([&file_o_name]).output().expect("Failed to remove intermediate linker file: ");
+
+    println!("Successfully compiled to {}!", &file_exe_name);
+
+    println!("Executing compiled program:\n");
+    let final_return = Command::new(String::from("./") + &file_exe_name).output().expect("Program execution failed: ");
+    println!("{}", &final_return.stdout.iter().map(|b| *b as char).collect::<String>());
+    println!("Process finished with exit code: {:?}", &final_return.status.code().unwrap());
+}
---
+++ test.ll
@@ -0,0 +1,10 @@
+
+
+declare i32 @puts(ptr)
+
+@str = constant [14 x i8] c"Hello, World!\00"
+
+define i32 @main(){
+call i32 @puts(ptr @str)
+ret i32 10
+}
